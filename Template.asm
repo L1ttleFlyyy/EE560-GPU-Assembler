c30d33b4af912d2b2b92dc60673eefcac8eda15d
L1ttleFlyyy <920114525@qq.com>
2020-01-14 16:41:47 -0800

    Update asm template

--- Template.asm
+++ Template.asm
@@ -3,6 +3,7 @@
 ;Summer 2016
 ;Topic: Matrix Multiplication
 ;Auther: Heqing Huang
+;Version: Lockless 
 ;-----------------------------------
 
 
@@ -66,10 +67,10 @@
 ;-----------------------------------
 ;$0 - Store 0;
 ;$1 - Store data of the element of Matrix A from memory 
-;$2 - Store data of the element of Matrix B from memory 
+;$2 - Store data of the element of Matrix B from memory
 ;$3 - Store the result of A X B
-;$4 - Store the lock
-;$5 - Store data of SUM from memory
+;$4 - Store the lock -- Since we do not need lock here there is no need for this register here but for simplicity we keep it here but we are not using it. 
+;$5 - Store data of SUM from memory 
 ;$6 - Store the barrier
 ;$7 - Store 16 to check barrier
 ;-----------------------------------
@@ -85,35 +86,86 @@
 ;#################################################################
 ; 							THREAD 0
 ;#################################################################
-
 ;Add your code here
+LW $1, 0($0); Load A
+LW $2, 64($0); Load B
+MULT $3, $1, $2; A x B
+LOCK_T0: LL $5, 132($0);
+ADD $5, $5, $3;
+SC $5, 132($0);
+BEQ $5, $0, LOCK_T0;
+Barrier_T0: LL $6, 136($0); Barrier
+ADDI $6, $6, 1;
+SC $6, 136($0);
+BEQ $6, $0, Barrier_T0;
+ADDI $7, $0, 16; Initialize $7 to 16
+LW $6, 136($0); load the barrier 
+BNE $7, $6, Sleep_T0;
 CACHE 10101, 128($0);
-Sleep_T0: J Sleep_T0; Keeping the thread sleeping after it finishes its work
+Sleep_T0: J Sleep_T0;
 
 
 	
 ;#################################################################
 ; 							THREAD 1
 ;#################################################################
-
 ;Add your code here
+LW $1, 4($0); Load A
+LW $2, 68($0); Load B
+MULT $3, $1, $2; A x B
+LOCK_T1: LL $5, 132($0);
+ADD $5, $5, $3;
+SC $5, 132($0);
+BEQ $5, $0, LOCK_T1;
+Barrier_T1: LL $6, 136($0); Barrier
+ADDI $6, $6, 1;
+SC $6, 136($0);
+BEQ $6, $0, Barrier_T1;
+ADDI $7, $0, 16; Initialize $7 to 16
+LW $6, 136($0); load the barrier 
+BNE $7, $6, Sleep_T1;
 CACHE 10101, 128($0);
-Sleep_T1: J Sleep_T1; Keeping the thread sleeping after it finishes its work
+Sleep_T1: J Sleep_T1;
 
 
 ;#################################################################
 ; 							THREAD 2
 ;#################################################################
-
 ;Add your code here
+LW $1, 8($0); Load A
+LW $2, 72($0); Load B
+MULT $3, $1, $2; A x B
+LOCK_T2: LL $5, 132($0);
+ADD $5, $5, $3;
+SC $5, 132($0);
+BEQ $5, $0, LOCK_T2;
+Barrier_T2: LL $6, 136($0); Barrier
+ADDI $6, $6, 1;
+SC $6, 136($0);
+BEQ $6, $0, Barrier_T2;
+ADDI $7, $0, 16; Initialize $7 to 16
+LW $6, 136($0); load the barrier 
+BNE $7, $6, Sleep_T2;
 CACHE 10101, 128($0);
-Sleep_T2: J Sleep_T2; Keeping the thread sleeping after it finishes its work
+Sleep_T2: J Sleep_T2;
 
 ;#################################################################
 ; 							THREAD 3
 ;#################################################################
-
 ;Add your code here
+LW $1, 12($0); Load A
+LW $2, 76($0); Load B
+MULT $3, $1, $2; A x B
+LOCK_T3: LL $5, 132($0);
+ADD $5, $5, $3;
+SC $5, 132($0);
+BEQ $5, $0, LOCK_T3;
+Barrier_T3: LL $6, 136($0); Barrier
+ADDI $6, $6, 1;
+SC $6, 136($0);
+BEQ $6, $0, Barrier_T3;
+ADDI $7, $0, 16; Initialize $7 to 16
+LW $6, 136($0); load the barrier 
+BNE $7, $6, Sleep_T3;
 CACHE 10101, 128($0);
-Sleep_T3: J Sleep_T3; Keeping the thread sleeping after it finishes its work
-
+Sleep_T3: J Sleep_T3;
