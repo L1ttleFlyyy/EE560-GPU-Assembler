fc4d7484751acead525b978113d9d8897e209f84
L1ttleFlyyy <920114525@qq.com>
2020-05-15 00:46:46 -0700

    Back to R-type

--- new.asm
+++ new.asm
@@ -1,6 +1,8 @@
 SUB $0, $0, $0; hard zero
-SHL $1, $16, 3; warpID * 8
-ADD $2, $8, $1; global thrID
-SHL $3, $2, 2; mem location = thrID * 4
-SW $3, 0($3); store thrID to corresponding location
+ADDI $1, $0, 3; temp register
+SHL $2, $16, $1; warpID * 8
+ADD $2, $8, $2; global thrID
+ADDI $1, $0, 2
+SHL $3, $2, $1; mem location = thrID * 4
+SW $2, 0($3); store thrID to corresponding location
 EXIT
